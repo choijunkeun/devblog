925a6e0f33033cd3bd627a185a837b42b4b50c88
jan <bonawillis@naver.com>
2024-02-06 01:31:27 +0900

    Fix: function Toc

--- src/components/archive/PostDetail.jsx
+++ src/components/archive/PostDetail.jsx
@@ -1,45 +1,79 @@
 import { useParams } from 'react-router-dom'
-import React, { useState, useEffect, useRef} from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
 import styles from './PostDetail.module.css'
 import { AiOutlineCalendar, AiOutlineFolderOpen } from "react-icons/ai";
-import MDEditor from '@uiw/react-md-editor';
 import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
 import rehypeSanitize from 'rehype-sanitize';
+import { Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import remarkGfm from 'remark-gfm';
 
-
-const HeadingElement = ( { node, children } ) => {
-    const ref = useRef(null);
-    useEffect(() => {
-    },[]);
-    return React.createElement(node.tagName, { ref }, children);
-}
+/**
+ * @description state 무한참조를 해결하지 못해 querySelectorAll로 구현해놓음(사용X)
+ */
+// const Heading = ({ node, children }) =>  {
+//     const ref = useRef(null);
+//     return React.createElement(node.tagName, { ref }, children);
+// }
 
 const PostDetail = () => {
     const {idx} = useParams();
     const [post, setPost] = useState({});
     const [refs, setRefs] = useState([]);
+
+    const observer = useRef();
+
+    // making..
     const [loading, setLoading] = useState(true);
 
-
-
-    const getPost = async () => {
-        const result = await axios.get(`http://localhost:80/post/${idx}`)
+    // 게시글 조회
+    const fetchPost = async () => {
+        await axios
+        .get(`http://localhost:80/post/${idx}`)
         .then((response) => {
             setPost(response.data);
             setLoading(false);
-            const mdWrap  = document.getElementById("markdown-wrap");
-            const elems = mdWrap.querySelectorAll('h1, h2, h3, h4, h5');
-            const elemsList = Array.from(elems);
-            setRefs(elemsList);
         });
-    };
+    }
 
-    useEffect(()=> {
-        getPost();
+    const setHeadScroll = () => {
+        const container = document.getElementById('markdown-container');
+        const headElem = container.querySelectorAll('h1, h2, h3, h4, h5');
+        setRefs(Array.from(headElem));
+    }
+
+    useEffect(() => {
+        fetchPost();
     }, []);
 
+    useEffect(()=> {
+        setHeadScroll();
+    },[post]);
+
+    // 뷰포트감지
+    useEffect(() => {
+        observer.current = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    highlightSection(entry.target);
+                }
+            });
+        }, { threshold: 0.1 }); // 요소 10% 화면에 보일 때 감지
+
+        refs.forEach((ref) => observer.current.observe(ref));
+
+        return ()=> {
+            refs.forEach((ref) => observer.current.unobserve(ref));
+        }
+    }, [refs]);
+
+    const highlightSection = (viewElement) => {
+        console.log(viewElement);
+
+    }
+
+    // Toc 버튼 이벤트
     const scrollToHeading = (element) => {
         element.scrollIntoView({ behavior: "smooth"})
     }
@@ -64,24 +98,54 @@ const PostDetail = () => {
                             <ul className={styles.tocMenu}>
                                     {
                                         refs.map((ref, index)=> {
+                                            let stylePedLeft = "";
+                                            switch (ref.tagName.toLowerCase()) {
+                                                case "h2":
+                                                    stylePedLeft = "1em";
+                                                    break;
+                                                case "h3":
+                                                    stylePedLeft = "1.75em";
+                                                    break;
+                                                case "h4":
+                                                    stylePedLeft = "2.5em";
+                                                    break;
+                                                case "h5":
+                                                    stylePedLeft = "3.25em";
+                                                    break;
+                                                default:
+                                                    break;
+                                            }
                                             return (
-                                                <li key={index} onClick={()=> scrollToHeading(ref)}>{ref.innerText}</li>
+                                                <li key={index} style={{ paddingLeft: stylePedLeft }} onClick={()=> scrollToHeading(ref)}>{ref.innerText}</li>
                                             )
                                         })
                                     }
                             </ul>
                         </div>
                     </div>
-                    <div className={styles.detailContent} id="markdown-wrap">
-                            <ReactMarkdown 
+                    <div className={styles.detailContent} id="markdown-container">
+                            <ReactMarkdown
                                 rehypePlugins={ [rehypeRaw, rehypeSanitize] }
+                                remarkPlugins={ [remarkGfm] }
                                 components={{
-                                    h1: HeadingElement,
-                                    h2: HeadingElement,
-                                    h3: HeadingElement,
-                                    h4: HeadingElement,
-                                    // h5: HeadingElement,
-                                }}>{post.content}
+                                    // h1: (props) => <Heading {...props} setRefs={setRefs} />,
+                                    img: ({node, ...props}) => <img style={{maxWidth: '100%'}}{...props} alt=""/>,
+                                    code({node, inline, className, children, ...props}) {
+                                        const match = /(\s*)/g.exec(className || '');
+                                        return !inline && match ? (
+                                            <SyntaxHighlighter 
+                                                {...props}
+                                                PreTag="div" 
+                                                children={String(children).replace(/\n$/, '')}
+                                                language={match[1]} 
+                                            />
+                                        ) : (
+                                          <code className={className} {...props}>
+                                            {children}
+                                          </code>
+                                        )
+                                      }
+                                    }}>{post.content}           
                             </ReactMarkdown>
                         <strong>
                             <div className={styles.detailFooter}>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
     }
 
     useEffect(()=> {
-        getCategories();
+        // getCategories();
     })
 
     const btnWritePage = () => {
